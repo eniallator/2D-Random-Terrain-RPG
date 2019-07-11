55569d548504625ebf9d454bc9112f70bf68aaf2
eniallator <eniallatormc@gmail.com>
2019-07-11 19:40:22 +0100

    Made the player's speed depend on the tps, rather than being a constant value

--- src/Player.lua
+++ src/Player.lua
@@ -1,6 +1,8 @@
+local config = require 'conf'
+
 local Entity = require 'src.Entity'
 local Sprite = require 'src.Sprite'
 
-local player = Entity(Sprite('assets/textures/icons/game-icon.png'), 5)
+local player = Entity(Sprite('assets/textures/icons/game-icon.png'), 100 / config.tps)
 
 return player
