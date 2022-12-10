1998aef9318e3cf25a9315992652e898f7d1bf22
eniallator <eniallatormc@gmail.com>
2022-12-10 16:13:11 +0000

    Made chunks use string builders now

--- server/environment/Chunk.lua
+++ server/environment/Chunk.lua
@@ -1,4 +1,5 @@
 local config = require 'conf'
+local StringBuilder = require 'common.utils.StringBuilder'
 
 return function(x, y, terrainGenerator)
     local chunk = {}
@@ -19,28 +20,27 @@ return function(x, y, terrainGenerator)
     end
 
     function chunk:getData()
-        local data
         if self._data == nil then
-            data = {
-                pos = self.pos,
-                groundTiles = ''
-            }
+            local groundTilesBuilder = StringBuilder()
             for i = 1, #self.groundTiles do
-                if data.groundTiles ~= '' then
-                    data.groundTiles = data.groundTiles .. '&'
+                if groundTilesBuilder.length > 0 then
+                    groundTilesBuilder:add('&')
                 end
                 local insertComma = false
                 for j = 1, #self.groundTiles[i] do
-                    data.groundTiles =
-                        data.groundTiles .. (insertComma and ',' or '') .. tostring(self.groundTiles[i][j])
+                    if insertComma then
+                        groundTilesBuilder:add(',')
+                    end
+                    groundTilesBuilder:add(tostring(self.groundTiles[i][j]))
                     insertComma = true
                 end
             end
-            self._data = data
-        else
-            data = self._data
+            self._data = {
+                pos = self.pos,
+                groundTiles = groundTilesBuilder:build()
+            }
         end
-        return data
+        return self._data
     end
 
     return chunk
